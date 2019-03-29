7e393d2d7f13f3b60e4b097232dd568dfc72b05b
Laszlo Megyer <lez@github.com>
2019-03-29 10:09:54 +0100

    wip: accounts page

---
+++ src/components/accounts.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    Accounts
+    <ul>
+      <li v-bind:key="i.site" v-for="i in this.$store.state.accounts">
+        {{ i.site }} - {{ i.handle }}
+      </li>
+    </ul>
+    <button v-on:click="console.log('hi')">Add</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AccountsPage'
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import MainPage from './components/index.vue'
+import AccountsPage from './components/accounts.vue'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'main',
       component: MainPage
+    },
+    {
+      path: '/accounts',
+      name: 'accounts',
+      component: AccountsPage
     }
   ]
 })
