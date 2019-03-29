c6aa84e45bf159501712983463c438cc9427491d
Laszlo Megyer <lez@github.com>
2019-03-29 12:31:07 +0100

    add header and footer to accounts page

--- src/components/accounts.vue
+++ src/components/accounts.vue
@@ -1,18 +1,30 @@
 <template>
   <div>
-    Accounts
+    <Header name="Account details"/>
     <ul>
       <li v-bind:key="i.site" v-for="i in this.$store.state.accounts">
         {{ i.site }} - {{ i.handle }}
       </li>
     </ul>
-    <button v-on:click="console.log('hi')">Add</button>
+    <button v-on:click="add">Add</button>
+    <Footer/>
   </div>
 </template>
 
 <script>
+import Header from '@/components/common/Header'
+import Footer from '@/components/common/Footer'
 export default {
-  name: 'AccountsPage'
+  name: 'AccountsPage',
+  components: {
+    Header,
+    Footer,
+  },
+  methods: {
+    add(){
+      alert('Adding account.');
+    }
+  },
 }
 </script>
 
