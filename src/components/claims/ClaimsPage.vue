554ae774f840c30f84f9e33f82888d82d3cd8ed1
Pooja Maheshwari <pooja.maheshwari247@gmail.com>
2019-04-11 10:26:59 +0200

    fixed app-title and added some css to claims page

--- src/components/claims/ClaimsPage.vue
+++ src/components/claims/ClaimsPage.vue
@@ -2,14 +2,16 @@
   <div>
     <div>
         <div class="page-title">Claims</div>
-        <input v-model="search" placeholder="Search in claims" type="text"/>
+        <input v-model="search" class='search-box' placeholder="Search in claims" type="text"/>
         <span class="claim-filter" v-if="type">
           {{ type }}
           <a v-on:click="remove_type_filter()"><i class="fa fa-minus-circle"/></a>
         </span>
-        <hr>
+        <!-- <hr> -->
         <div class="claim" v-bind:key="item.id" v-for="item in filtered_list">
           <a v-on:click="add_type_filter(item.type)">{{ item.type }}</a> - {{ item.summary }}
+          <br/>
+          <div class="all-details" v-for="(val,index) in fetchKey(item)" :key="index">{{item[val] + " "}}</div>
         </div>
     </div>
   </div>
@@ -33,6 +35,9 @@ export default {
     remove_type_filter: function() {
       this.type = null
     },
+    fetchKey: function (obj) {
+     return Object.keys(obj)
+    }
   },
   computed: {
     filtered_list() {
@@ -63,4 +68,13 @@ export default {
 </script>
 
 <style scoped>
+.search-box{
+  width: 100%;
+  padding: 12px 16px;
+  margin: 0 0 20px 0px;
+  outline: none;
+}
+.all-details{
+  display: inline;
+}
 </style>
--- src/components/overview/ProfileList.vue
+++ src/components/overview/ProfileList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div v-for="(keyvault, index1) in keyVaults" v-bind:key="index1">
-      <div class="keyvault-title">{{fetchKey(keyvault)}}</div>
+      <div class="keyvault-title cursor-default" :title="fetchTitle(fetchKey(keyvault))">{{fetchKey(keyvault)}}</div>
       <div class="display-inline-flex">
         <div v-for="(row, index2) in keyvault[fetchKey(keyvault)]" v-bind:key="index2">
           <ProfileCard :card-details="row"/>
@@ -175,6 +175,17 @@ export default {
     fetchKey: function (obj) {
      return Object.keys(obj)[0]
     },
+    fetchTitle: function (key) {
+      if (key == 'Devices') {
+        return "Devices that claim verifiably that they are connected to the profiles"
+      } else if (key == 'Profile') {
+        return "Profiles represent a behaviour "
+      } else if (key == 'Groups') {
+        return "Group is a group of profiles that have a representative"
+      } else if (key == 'Wallets') {
+        return "Wallets are specific profiles with a BIP44 or another pattern"
+      }
+    }
   },
 }
 </script>
