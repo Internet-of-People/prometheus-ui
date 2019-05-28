1accb94cb3ca093499e248f5b0fb6d156e88c220
Pooja Maheshwari <pooja.maheshwari247@gmail.com>
2019-05-28 20:55:45 +0530

    1. Removed all unused components and routes 2. Added breadcrumb to routing 3. added route params

---
+++ src/components/claims/ClaimsNew.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      New Claims
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ClaimsNew',
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/router.js
+++ src/router.js
@@ -1,56 +1,37 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Overview from './components/overview/ProfileList'
-import ProfileInfluencer from './components/overview/ProfileInfluencer'
-import ProfileStudent from './components/overview/ProfileStudent'
-import Profile from './components/overview/Profile'
-import Register from './components/register/Register'
-import ClaimsPage from './components/claims/ClaimsPage'
-import ProfileRegister from './components/overview/ProfileCreate/ProfileRegister'
+import ClaimsPage from '@/components/claims/ClaimsPage'
+import ClaimsNew from '@/components/claims/ClaimsNew'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'main',
-      component: Overview
-    },
-    {
-      path: '/profile/student',
-      name: 'profilestudent',
-      component: ProfileStudent
-    },    
-    {
-      path: '/profile/influencer',
-      name: 'profileinfluencer',
-      component: ProfileInfluencer
-    },
-    {
-      path: '/profile/new',
-      name: 'createprofile',
-      component: ProfileRegister
-    },
-    {
-      path: '/profile/:profile_id',
-      name: 'profile',
-      component: Profile
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: Register
-    },
-    {
       path: '/claims',
-      name: 'claims',
+      name: 'Claims',
+      component: ClaimsPage,
+      meta: {
+        breadcrumb: [
+          { name: 'Claims' }
+        ]
+      }
+    },
+    {
+      path: '/claims?:filter',
+      name: 'claimsfiltered',
       component: ClaimsPage
     },
     {
-      path: '/claims/:filter',
-      name: 'claimsfiltered', // cant name filtered list the same; gives duplicate errors
-      component: ClaimsPage
-    },
+      path: '/claimsnew',
+      name: 'NewClaims',
+      component: ClaimsNew,
+      meta: {
+        breadcrumb: [
+          { name: 'Claims', link: 'claims' },
+          { name: 'New Claims' }
+        ]
+      }
+    }
   ]
 })
