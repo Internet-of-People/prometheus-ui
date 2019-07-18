7fb7751f6ce8c8f25e58dcc907fdcdf6299e7e4b
wigy <wigy_opensource_developer@yahoo.com>
2019-07-18 14:32:39 +0200

    One can give a good 1st impression only once

--- src/views/Introduction.vue
+++ src/views/Introduction.vue
@@ -1,7 +1,7 @@
 <template>
   <b-container fluid>
     <b-row class="text-center justify-content-md-center" align-h="center">
-      <b-col sm="2" lg="12">
+      <b-col cols="6" md="4" lg="2">
         <b-img :src="require('@/assets/images/logo.svg')" fluid alt="Responsive image" />
       </b-col>
     </b-row>
@@ -9,30 +9,38 @@
       <b-col><h1>{{ appName }}</h1></b-col>
     </b-row>
     <b-row class="justify-content-md-center mt-4">
-      <b-col sm="12" lg="8">
+      <b-col cols="12" lg="8">
         Prometheus uses a vault generated from a seed phrase similar to cryptocurrency hardware
-        wallets. This seed is the key to all your data and should you lose it, you lose access
-        to all your data, currency and relations.
+        wallets. <strong>This seed is the key to all your data and should you lose it, you lose
+        access to all your data, currency and relations.</strong>
         <br>Keep the phrase somewhere safe, preferably multiple copies, split into multiple pieces
         at different locations.
       </b-col>
     </b-row>
-    <b-row class="justify-content-md-center mt-4 text-center">
-      <b-col sm="12" lg="8">
+    <b-row class="justify-content-center mt-4 text-center">
+      <b-col cols="12" lg="8">
         <b-card-group deck>
-          <b-card>
-            <b-card-text class="text-muted">
-              Choose this option if you have no seeds to restore.
-              It will generate a new vault with a randomly generated seed.
-            </b-card-text>
-            <b-button @click="createNewVault" variant="primary">CREATE NEW VAULT</b-button>
+          <b-card no-body>
+            <b-card-body>
+              <b-card-text class="text-muted">
+                Choose this option if you have no seeds to restore.
+                It will generate a new vault with a randomly generated seed.
+              </b-card-text>
+            </b-card-body>
+            <b-card-footer footer-bg-variant="white">
+              <b-button @click="createNewVault" variant="primary">CREATE NEW VAULT</b-button>
+            </b-card-footer>
           </b-card>
-          <b-card sm="6" lg="4">
-            <b-card-text class="text-muted">
-              If you have an already generated seed and want to restore your vault,
-              this is what you need.
-            </b-card-text>
-            <b-button @click="restoreVault" variant="light">RESTORE VAULT</b-button>
+          <b-card no-body>
+            <b-card-body>
+              <b-card-text class="text-muted">
+                If you have an already generated seed and want to restore your vault,
+                this is what you need.
+              </b-card-text>
+            </b-card-body>
+            <b-card-footer footer-bg-variant="white">
+              <b-button @click="restoreVault" variant="light">RESTORE VAULT</b-button>
+            </b-card-footer>
           </b-card>
         </b-card-group>
       </b-col>
